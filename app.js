c823ded333450466346d33789efdba87cd8b3383
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-07-14 19:42:23 -0400

    index page. About to set up user collection

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/photoVault");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var userSchema = new mongoose.Schema({
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    password: string
+});
+
+var User = mongoose.model("user", userSchema);
+ 
 
 app.get("/", function(req, res){
-    res.send("Hello there!");
-})
-
-app.get("/bye", function(req, res){
-    res.send("Goodbye!");
+    res.render("index.ejs");
 })
 
 app.get("*", function(req, res){
