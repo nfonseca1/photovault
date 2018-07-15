32533a864eacd5cc7f3634ef6a59f2854c39cf54
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-07-15 13:15:05 -0400

    Sign in form adds new user and sends to home page.

--- app.js
+++ app.js
@@ -7,18 +7,41 @@ mongoose.connect("mongodb://localhost/photoVault");
 app.use(bodyParser.urlencoded({extended: true}));
 
 var userSchema = new mongoose.Schema({
-    firstName: string,
-    lastName: string,
-    username: string,
-    email: string,
-    password: string
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    password: String
 });
 
 var User = mongoose.model("user", userSchema);
- 
+
 
 app.get("/", function(req, res){
     res.render("index.ejs");
+})
+
+app.get("/home", function(req, res){
+    res.render("home.ejs");
+})
+
+app.post("/home", function(req, res){
+    var newUser = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password1
+    }
+    User.create(newUser, function(err, user){
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("new user " + user)
+            res.redirect("/home");
+        }
+    })
 })
 
 app.get("*", function(req, res){
