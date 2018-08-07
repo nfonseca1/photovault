a874c4eb3fa802a326e9eeaf7788664f27c8abe2
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-08-06 22:35:28 -0400

    Added edit button for each comment. Edit form appears when clicked. Not yet working.

--- public/comment.js
+++ public/comment.js
@@ -1,11 +1,29 @@
 var commentArea = document.querySelector("#commentArea");
 var commentButton = document.querySelector("#addComment");
+var commentSections = document.querySelectorAll(".comment");
+var addFormOn = false;
 
 commentButton.addEventListener("click", openCommentForm);
+commentSections.forEach(function(commentSection) {
+    commentSection.children[1].addEventListener("click", function(e){
+        var html = "";
+        html += '<form action="/home/<%=post._id%>/comment/'+ commentSection.getAttribute('data-comment-id') +'?_method=PUT" method="POST">';
+        html += '<input type="text" name="comment[text]" value="'+ commentSection.getAttribute('data-comment-text') +'">';
+        html += '</form>';
+        commentSection.innerHTML = html;
+    })
+});
 
 function openCommentForm(){
-    var html = '<form action="/home/<%=post[0]._id%>" method="POST">';
-    html += '<input type="text" name="text">';
-    html += '</form>';
-    commentArea.innerHTML = html;
+    var html = "";
+    if(addFormOn) {
+        commentArea.innerHTML = "";
+        addFormOn = false;
+    } else {
+        html = '<form action="/home/<%= post._id %>" method="POST">';
+        html += '<input type="text" class="form-control" name="comment[text]">';
+        html += '<button type="submit" class="btn btn-xs btn-success">Post</button></form>';
+        commentArea.innerHTML = html;
+        addFormOn = true;
+    }
 }
