e9c12f359844e0fa8da4c26a9bdeeb1e43414ec1
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-09-21 16:48:30 -0400

    media query update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 app.use(express.static(__dirname +'/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(require("express-session")({
+app.use(require("express- session")({
     secret: "If you only knew the power of the dark side.",
     resave: false,
     saveUninitialized: false
